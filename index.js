697e8036c887a2d8ac2aaa2a98f8bd4172ae2917
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-02-14 01:58:08 +0500

    refactor: imported db as module and created api for getting data from db

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const db = require("./database/db");
 
 const app = express();
 const port = 5000;
+
+app.get("/api/get", (req, res) => {
+  const sqlInsert = "SELECT * FROM users_table";
+  db.query(sqlInsert, (error, result) => {
+    res.send(result)
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
