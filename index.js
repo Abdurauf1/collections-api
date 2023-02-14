2527a714bbabdc4f0ea041f82ac64436f044f9d0
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-02-14 21:21:17 +0500

    feat: add cors package

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const usersRoutes = require("./routes/usersRoutes");
+const cors = require("cors");
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
+app.use(express.json());
 app.use("/api", usersRoutes);
 
 app.get("/", (req, res) => {
